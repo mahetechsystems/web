194ef478a10546f4c9d96fa902924c3a859f0848
Ch Mahesh <chmahesh.dev@gmail.com>
2026-02-16 17:52:16 +0530

    test: add Vitest configuration and setup

---
+++ vitest.setup.ts
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+
+// Mock IntersectionObserver for components that use scroll animations
+global.IntersectionObserver = class IntersectionObserver {
+  constructor() {}
+  disconnect() {}
+  observe() {}
+  takeRecords() {
+    return [];
+  }
+  unobserve() {}
+} as any;
+
